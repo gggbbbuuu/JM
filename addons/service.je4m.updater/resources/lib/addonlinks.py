12995d6fbfd7d59a3a34887d90a8714613c01cf0
gggbbbuuu <57154026+gggbbbuuu@users.noreply.github.com>
2022-05-11 17:25:56 +0300

    Build service update 19.1.3

--- addons/service.je4m.updater/resources/lib/addonlinks.py
+++ addons/service.je4m.updater/resources/lib/addonlinks.py
@@ -1,12 +1,14 @@
 
-ADDONS_REPOS = [('service.je4m.updater',               'repository.gkobu'),
+ADDONS_REPOS = [('service.je4m.updater',                'repository.gkobu'),
                 ('plugin.program.downloader',           'repository.gkobu'),
                 ('plugin.program.autocompletion',       'repository.gkobu'),
                 ('plugin.video.cartoonsgr',             'repository.gkobu'),
-                ('plugin.video.fmoviesto',             'repository.gkobu'),
+                ('plugin.video.fmoviesto',              'repository.gkobu'),
+                ('plugin.video.theoath',                'repository.gkobu'),
+                ('script.module.oathscrapers',          'repository.gkobu'),
                 ('script.extendedinfo',                 'repository.newdiamond')
                  ]
 
 URLS = ['http://gknwizard.eu/repo/Builds/GKoBu/GKoBuM/gkobu_rescue.zip']
 
-REMOVELIST = []
+REMOVELIST = ['repository.host505']
