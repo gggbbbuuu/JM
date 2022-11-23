2aa23f19b4b7b4b73ec96b045706f636684ddd64
gggbbbuuu <57154026+gggbbbuuu@users.noreply.github.com>
2022-11-23 13:36:51 +0200

    updates 23/11/22

--- addons/script.module.resolveurl/lib/resolveurl/plugins/watchingvn.py
+++ addons/script.module.resolveurl/lib/resolveurl/plugins/watchingvn.py
@@ -1,6 +1,6 @@
 """
     Plugin for ResolveURL
-    Copyright (C) 2021 gujal
+    Copyright (C) 2022 shellc0de
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -16,19 +16,18 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
 from resolveurl.lib import helpers
+from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
 
 
-class VidMXResolver(ResolveGeneric):
-    name = 'VidMX'
-    domains = ['vidmx.xyz', 'vembx.one']
-    pattern = r'(?://|\.)((?:vidmx|vembx)\.(?:xyz|one))/(?:embed\d?-)?([0-9a-zA-Z]+)'
+class WatchingVnResolver(ResolveGeneric):
+    name = 'WatchingVn'
+    domains = ['watching.vn']
+    pattern = r'(?://|\.)(watching\.vn)/(?:embed-|e/|d/)?([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
         return helpers.get_media_url(
             self.get_url(host, media_id),
-            patterns=[r'''{file:"(?P<url>[^"]+)",label:"(?P<label>[^"]+)"'''],
-            generic_patterns=False,
-            referer=False
+            patterns=[r'''sources:\s*\[{src:\s*["'](?P<url>[^"']+)'''],
+            generic_patterns=False
         )
